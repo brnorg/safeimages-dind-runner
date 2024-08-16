ee392dce810bfa4c90c6e854eac5f7ebb456d8dc
brn <56989493+brunosoouza@users.noreply.github.com>
2024-08-16 17:24:34 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,27 +1,44 @@
 #!/bin/bash
 
-export ORGANIZATION=$ORG
-export ACCESS_TOKEN=$TOKEN
-export TAG_RUNNER=$TAG
+# Check if the script is running for an organization or a repository
+if [ -n "$ORG" ]; then
+    # Organization scenario
+    export ORGANIZATION=$ORG
+    export ACCESS_TOKEN=$TOKEN
+    export TAG_RUNNER=$TAG
 
-echo "ORGANIZATION ${ORGANIZATION}"
-echo "ACCESS_TOKEN ${ACCESS_TOKEN}"
+    echo "ORGANIZATION ${ORGANIZATION}"
+    echo "ACCESS_TOKEN ${ACCESS_TOKEN}"
 
-REG_TOKEN=$(curl -k -L -X POST \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer ${ACCESS_TOKEN}" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
+    REG_TOKEN=$(curl -k -L -X POST \
+      -H "Accept: application/vnd.github+json" \
+      -H "Authorization: Bearer ${ACCESS_TOKEN}" \
+      -H "X-GitHub-Api-Version: 2022-11-28" \
+      https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
 
-echo "REG_TOKEN ${REG_TOKEN}"
+    echo "REG_TOKEN ${REG_TOKEN}"
 
-cd /home/runner/actions-runner
+    cd /home/runner/actions-runner
 
-./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN} --labels ${TAG_RUNNER}
+    ./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN} --labels ${TAG_RUNNER}
+else
+    # Repository scenario
+    REPOSITORY=$REPO
+    ACCESS_TOKEN=$TOKEN
+
+    echo "REPO ${REPOSITORY}"
+    echo "ACCESS_TOKEN ${ACCESS_TOKEN}"
+
+    REG_TOKEN=$(curl -k -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
+
+    cd /home/runner/actions-runner
+
+    ./config.sh --url https://github.com/${REPOSITORY} --token ${REG_TOKEN}
+fi
 
 cleanup() {
     echo "Removing runner..."
-    ./config.sh remove --token ${REG_TOKEN}
+    ./config.sh remove --unattended --token ${REG_TOKEN}
 }
 
 trap 'cleanup; exit 130' INT
